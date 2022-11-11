0770c472e354ce62614baf48407574220ba573ce
Cody Nhan Pham <87593612+codynhanpham@users.noreply.github.com>
2022-11-11 15:38:09 -0600

    ehhh

---
+++ PWAserviceWorker.js
@@ -0,0 +1,23 @@
+const staticClock = "PWA-Clocksite-v1";
+const assets = [
+    "/",
+    "/index.html",
+    "/css/clockstyle.css",
+    "/js/clockscript.js",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticClock).then(cache => {
+        cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
